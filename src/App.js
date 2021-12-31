f44527cd2c0c268501bfcf251e8a84c731387269
tapas8871 <90463815+tapas8871@users.noreply.github.com>
2021-12-31 15:41:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Temp from './weather/Temp'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Temp/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/weather/Temp.js
@@ -0,0 +1,82 @@
+// https://api.openweathermap.org/data/2.5/weather?q=bhopal&appid=5ffa5ad4996e460844d4a8c6c7e410ad
+
+import "./style.css";
+import React, { useState, useEffect } from "react";
+import Weathercard from "./Weathercard";
+import "./style.css";
+
+// const inputId = document.getElementById('search');
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("bhopal");
+  const [tempInfo, setTempInfo] = useState({});
+  
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=5ffa5ad4996e460844d4a8c6c7e410ad`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // inputId.addEventListener('keydown',function(e){
+  //   if(e.code === "Enter"){
+  //     getWeatherInfo();
+  //   }
+  // });
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card  */}
+      <Weathercard {...tempInfo} />
+    </>
+  );
+};
+
+export default Temp;
